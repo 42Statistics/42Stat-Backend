ae9c7387887208436f5cf9bb2cd3196f31daf48e
jpham005 <jpham0005@gmail.com>
2023-03-22 21:15:24 +0900

    revert: :rewind: 기존 작업하던 것을 추후 public api 작업으로 분리, 42stat 서비스용 api로 재설계 시작
    새 마음 새 뜻...

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -2,15 +2,9 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { ScaleTeamsModule } from './scaleTeams/scaleTeams.module';
-import { TeamsModule } from './teams/teams.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    UsersModule,
-    TeamsModule,
-    ScaleTeamsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       buildSchemaOptions: {
