78c8cb38321079dee1235eb714ae2e2358528a89
yeju <74657997+niamu01@users.noreply.github.com>
2023-04-22 12:59:22 +0900

    Revert "feat: :sparkles: env 적용"
    This reverts commit c0e863738b77068d973286633456e58379ebcb85.

--- app/src/app.module.ts
+++ app/src/app.module.ts
@@ -15,15 +15,19 @@ import { ProjectModule } from './project/project.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_ENDPOINT}`,
+      'mongodb://rootstat:ValidPassw0rd6373@stat-mongodb-cluster.cluster-ci2xhynknqpo.ap-northeast-2.docdb.amazonaws.com:27017',
       {
-        tlsCAFile: `${process.env.DB_TLS_CA_PATH}`,
+        tlsCAFile: '/tmp/rds-combined-ca-bundle.pem',
         tls: true,
         replicaSet: 'rs0',
         readPreference: 'secondaryPreferred',
         retryWrites: false,
       },
     ),
+    //todo: for local
+    //MongooseModule.forRoot(
+    //  'mongodb://42statuser:0214@host.docker.internal:27017/42stat',
+    //),
     ProjectModule,
     HomeModule,
     TotalModule,
