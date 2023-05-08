b0513a5824fa168257625bb663829efb83aa0096
jaham <jpham0005@gmail.com>
2023-05-08 14:56:53 +0900

    feat: :sparkles: team api
    total module 과의 연계는 cursus_users 작업이 끝나면 하는걸로 하겠습니다. 그 외에도 scale team과 exam api 와 연관하여 해야할 부분들이 남아있습니다.
    
    - #79

---
+++ app/src/team/team.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TeamSchema, team } from './db/team.database.schema';
+import { TeamService } from './team.service';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: team.name, schema: TeamSchema }]),
+  ],
+  providers: [TeamService],
+  exports: [MongooseModule, TeamService],
+})
+export class TeamModule {}
