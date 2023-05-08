2af3274522748b9d12282c90f52bd4f88824c9c1
jaham <jpham0005@gmail.com>
2023-05-08 14:56:53 +0900

    feat: :sparkles: team api
    total module 과의 연계는 cursus_users 작업이 끝나면 하는걸로 하겠습니다. 그 외에도 scale team과 exam api 와 연관하여 해야할 부분들이 남아있습니다.
    
    - #79

---
+++ app/src/team/team.service.ts
@@ -0,0 +1,67 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { team } from './db/team.database.schema';
+import { FilterQuery, Model } from 'mongoose';
+import { AggrNumeric } from 'src/common/db/common.db.aggregation';
+
+@Injectable()
+export class TeamService {
+  constructor(
+    @Inject(team.name)
+    private teamModel: Model<team>,
+  ) {}
+
+  async getTeamCount(filter?: FilterQuery<team>): Promise<number> {
+    if (!filter) {
+      return await this.teamModel.estimatedDocumentCount();
+    }
+
+    return await this.teamModel.countDocuments(filter);
+  }
+
+  /**
+   *
+   * @return number[] 0번째에 pass, 1번째에 fail 숫자를 담은 배열을 반환합니다.
+   */
+  async getTeamResult(filter?: FilterQuery<team>): Promise<[number, number]> {
+    const aggregate = this.teamModel.aggregate<
+      { _id: 'true' | 'false' } & AggrNumeric
+    >();
+
+    const teamResultAggr = await aggregate
+      .match({ ...filter, status: 'finished' })
+      .group({ _id: '$validated?', value: { $count: {} } });
+
+    const pass = teamResultAggr.find(({ _id }) => _id === 'true')?.value ?? 0;
+    const fail = teamResultAggr.find(({ _id }) => _id === 'false')?.value ?? 0;
+
+    return [pass, fail];
+  }
+
+  async getRegisteredCount(filter?: FilterQuery<team>): Promise<number> {
+    const aggregate = this.teamModel.aggregate<AggrNumeric>();
+
+    if (filter) {
+      aggregate.match(filter);
+    }
+
+    const registeredCountAggr = await aggregate.group({
+      _id: 'result',
+      value: { $sum: { $size: '$users' } },
+    });
+
+    return registeredCountAggr.length ? registeredCountAggr[0].value : 0;
+  }
+
+  async getTeamMatesUid(
+    targetUid: number,
+  ): Promise<{ uid: number; value: number }[]> {
+    const aggregate = this.teamModel.aggregate<{ uid: number } & AggrNumeric>();
+
+    return await aggregate
+      .match({ 'users.id': targetUid })
+      .unwind({ path: 'users' })
+      .group({ _id: '$users.id', value: { $count: {} } })
+      .match({ _id: { $ne: targetUid } })
+      .project({ _id: 0, uid: '$_id', value: 1 });
+  }
+}
