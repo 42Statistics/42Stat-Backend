7abc64a77fd707661b202f2dbc0f0d4ed9bf3ace
niamu01 <niamu01@naver.com>
2023-05-24 22:54:49 +0900

    fix: :ambulance: personalEval에 login, uid로 검색하기 기능 추가
    - #99

--- app/src/personalEval/personal.eval.resolver.ts
+++ app/src/personalEval/personal.eval.resolver.ts
@@ -1,28 +1,46 @@
-import { Args, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { Args, Context, Query, ResolveField, Resolver } from '@nestjs/graphql';
+import { CursusUserService } from 'src/cursusUser/cursusUser.service';
 import { UserProfile } from 'src/personalGeneral/models/personal.general.userProfile.model';
 import { PersonalGeneralService } from 'src/personalGeneral/personal.general.service';
 import { PersonalEval } from './models/personal.eval.model';
 import { PersonalEvalService } from './personal.eval.service';
 
+type PersonalEvalContext = { uid: number };
 @Resolver((_of: unknown) => PersonalEval)
 export class PersonalEvalResolver {
   constructor(
     private personalEvalService: PersonalEvalService,
     private personalGeneralService: PersonalGeneralService,
+    private cursusUserService: CursusUserService,
   ) {}
 
   @Query((_returns) => PersonalEval)
-  async getPersonalEvalPage(@Args('uid') uid: number): Promise<PersonalEval> {
+  async getPersonalEvalPage(
+    @Args('uid', { nullable: true }) uid: number,
+    @Args('login', { nullable: true }) login: string,
+    @Context() context: PersonalEvalContext,
+  ): Promise<PersonalEval> {
+    const cursusUser = await this.cursusUserService.findUser(uid, login);
+    context.uid = cursusUser.user.id;
+
     return {
-      currMonthCount: await this.personalEvalService.currMonthCount(uid),
-      lastMonthCount: await this.personalEvalService.lastMonthCount(uid),
-      totalCount: await this.personalEvalService.totalCount(uid),
-      averageDuration: await this.personalEvalService.averageDuration(uid),
-      averageFinalMark: await this.personalEvalService.averageFinalMark(uid),
+      currMonthCount: await this.personalEvalService.currMonthCount(
+        context.uid,
+      ),
+      lastMonthCount: await this.personalEvalService.lastMonthCount(
+        context.uid,
+      ),
+      totalCount: await this.personalEvalService.totalCount(context.uid),
+      averageDuration: await this.personalEvalService.averageDuration(
+        context.uid,
+      ),
+      averageFinalMark: await this.personalEvalService.averageFinalMark(
+        context.uid,
+      ),
       averageFeedbackLength:
-        await this.personalEvalService.averageFeedbackLength(uid),
+        await this.personalEvalService.averageFeedbackLength(context.uid),
       averageCommentLength: await this.personalEvalService.averageCommentLength(
-        uid,
+        context.uid,
       ),
     };
   }
