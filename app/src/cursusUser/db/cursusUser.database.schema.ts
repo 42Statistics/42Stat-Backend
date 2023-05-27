7e64d3c2ada1e631436d9b7e9894902f3962f0c4
niamu01 <niamu01@naver.com>
2023-05-27 14:40:15 +0900

    refactor: :recycle: pull requests기반 리팩토링
    PreferredTime타입 사용, dateDiff 매개변수 string으로 변경, clusterList 함수 밖에서 선언, getPreferredTime의 aggregate에서 계산 마치기, 리턴방식 맞추기, "null"반환
    
    - #98

--- app/src/cursusUser/db/cursusUser.database.schema.ts
+++ app/src/cursusUser/db/cursusUser.database.schema.ts
@@ -23,8 +23,8 @@ export class Image {
 export class User {
   @Prop({ required: true })
   id: number;
-  @Prop({ required: true })
-  email: string;
+  // @Prop({ required: true })
+  // email: string;
   @Prop({ required: true })
   login: string;
   //firstName: string;
