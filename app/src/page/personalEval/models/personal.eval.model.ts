e56c59b7585ed3143c1450fc53dd8169a387f2b2
niamu01 <niamu01@naver.com>
2023-05-28 18:22:40 +0900

    feat: :sparkles: personal eval page의 새로운 쿼리 추가
    - #103

--- app/src/page/personalEval/models/personal.eval.model.ts
+++ app/src/page/personalEval/models/personal.eval.model.ts
@@ -2,17 +2,6 @@ import { Field, Float, ObjectType } from '@nestjs/graphql';
 import { NumberDateRanged } from 'src/common/models/common.number.dateRanaged';
 import { UserProfile } from 'src/page/personalGeneral/models/personal.general.userProfile.model';
 
-// export enum EvalUserDifficulty {
-//   EASY,
-//   MEDIUM,
-//   HARD,
-//   HELL,
-// }
-
-// registerEnumType(EvalUserDifficulty, {
-//   name: 'EvalUserDifficulty',
-// });
-
 // @ObjectType()
 // export class DestinyUser extends UserPreview {
 //   @Field()
@@ -21,35 +10,42 @@ import { UserProfile } from 'src/page/personalGeneral/models/personal.general.us
 
 @ObjectType()
 export class PersonalEval {
-  @Field()
-  currMonthCount: NumberDateRanged;
-
-  @Field()
-  lastMonthCount: NumberDateRanged;
-
-  @Field()
-  totalCount: number;
-
-  @Field()
-  averageDuration: number;
-
-  @Field((_type) => Float, {
-    description: '평가자 기준으로 준 평균 점수 입니다.',
-  })
-  averageFinalMark: number;
-
-  @Field({ description: '피평가자가 작성한 리뷰의 평균 길이 입니다.' })
-  averageFeedbackLength: number;
-
-  @Field({ description: '평가자가 작성한 리뷰의 평균 길이 입니다.' })
-  averageCommentLength: number;
-
   @Field((_type) => UserProfile)
   userProfile: UserProfile;
 
-  //   @Field((_type) => EvalUserDifficulty)
-  // difficulty: EvalUserDifficulty;
+  @Field({ description: '보유 평가 포인트' })
+  correctionPoint: number;
+
+  @Field({ description: '월간 평가 횟수 (이번달 + 저번달)' })
+  currMonthCount: NumberDateRanged;
+
+  @Field({ description: '월간 평가 횟수 (이번달 + 저번달)' })
+  lastMonthCount: NumberDateRanged;
+
+  @Field({ description: '누적 평가 횟수' })
+  totalCount: number;
+
+  @Field({ description: '누적 평가 시간' })
+  totalDuration: number;
+
+  @Field({ description: '평균 평가 시간' })
+  averageDuration: number;
+
+  @Field((_type) => Float, { description: '평균 평가 점수' })
+  averageFinalMark: number;
+
+  @Field({ description: '평균 피드백 길이' })
+  averageFeedbackLength: number;
+
+  @Field({ description: '평균 코멘트 길이' })
+  averageCommentLength: number;
 
   // @Field((_types) => [DestinyUser], { nullable: 'items' })
   // destinyUsers: DestinyUser[];
+
+  @Field({ description: '최근 받은 피드백' })
+  latestFeedback: string;
+
+  @Field({ description: '이 유저의 이전 평가가 궁금하다면?' })
+  evalLogSearchLink: string;
 }
