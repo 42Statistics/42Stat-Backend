cc7bb4b45580b41acf7e8132741130ea44b373b6
jaham <jpham0005@gmail.com>
2023-06-09 18:56:27 +0900

    refactor: :recycle: IntRate tuple => Rate type
    프론트에서 사용하기 더 편할 것으로 사료되기도 하고, 백엔드에서도 장기적으로 더 적합한 타입이라고 판단되었습니다.

--- app/src/common/models/common.rate.model.ts
+++ app/src/common/models/common.rate.model.ts
@@ -1,10 +1,19 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 
 @ObjectType()
-export class IntRate {
+export class Pair {
   @Field()
-  total: number;
+  key: string;
 
   @Field()
   value: number;
+}
+
+@ObjectType()
+export class Rate {
+  @Field()
+  total: number;
+
+  @Field((_type) => [Pair])
+  fields: Pair[];
 }
