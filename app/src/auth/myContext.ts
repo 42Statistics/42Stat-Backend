a8115b783f5ffda9de6bbc1de931b426cc681e80
niamu01 <niamu01@naver.com>
2023-06-09 15:01:54 +0900

    style: :art: lint 적용
    - #106

--- app/src/auth/myContext.ts
+++ app/src/auth/myContext.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
 export const MyContext = createParamDecorator(
-  (data: unknown, context: ExecutionContext): number => {
+  (_data: unknown, context: ExecutionContext): number => {
     const gqlContext = GqlExecutionContext.create(context);
     return gqlContext.getContext().req.user.userId;
   },
