18135861f35485214294c5e3e487f1ea5da7fabc
jaham <jpham0005@gmail.com>
2023-06-13 09:55:10 +0900

    feat: :sparkles: leaderboard level cache 추가
    - #154

--- app/src/page/leaderboard/level/leaderboard.level.module.ts
+++ app/src/page/leaderboard/level/leaderboard.level.module.ts
@@ -1,18 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CursusUserModule } from 'src/api/cursusUser/cursusUser.module';
 import { CursusUserService } from 'src/api/cursusUser/cursusUser.service';
+import { RedisModule, RedisProvider } from 'src/redis/redis.module';
+import { RedisUtilService } from 'src/redis/redis.util.service';
 import { LeaderboardUtilModule } from '../util/leaderboard.util.module';
 import { LeaderboardUtilService } from '../util/leaderboard.util.service';
+import { LeaderboardLevelCacheService } from './leaderboard.level.cache.service';
 import { LeaderboardLevelResolver } from './leaderboard.level.resovler';
 import { LeaderboardLevelService } from './leaderboard.level.service';
 
 @Module({
-  imports: [LeaderboardUtilModule, CursusUserModule],
+  imports: [LeaderboardUtilModule, CursusUserModule, RedisModule],
   providers: [
     LeaderboardLevelResolver,
     LeaderboardLevelService,
+    LeaderboardLevelCacheService,
     LeaderboardUtilService,
     CursusUserService,
+    RedisProvider,
+    RedisUtilService,
   ],
 })
 // eslint-disable-next-line
