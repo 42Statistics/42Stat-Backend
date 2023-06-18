258271e1f633fb64976ad504d94ce617f43532a7
niamu01 <niamu01@naver.com>
2023-06-18 22:46:12 +0900

    feat: ✨ examResult 추가
    - close #178

--- app/src/page/home/team/home.team.module.ts
+++ app/src/page/home/team/home.team.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ExamModule } from 'src/api/exam/exam.module';
+import { ProjectsUserModule } from 'src/api/projectsUser/projectsUser.module';
+import { TeamModule } from 'src/api/team/team.module';
+import { DateRangeModule } from 'src/dateRange/dateRange.module';
 import { HomeTeamResolver } from './home.team.resolver';
 import { HomeTeamService } from './home.team.service';
-import { ProjectsUserModule } from 'src/api/projectsUser/projectsUser.module';
 
 @Module({
-  imports: [ProjectsUserModule],
+  imports: [ProjectsUserModule, TeamModule, ExamModule, DateRangeModule],
   providers: [HomeTeamResolver, HomeTeamService],
 })
 // eslint-disable-next-line
--- app/src/page/home/team/home.team.service.ts
+++ app/src/page/home/team/home.team.service.ts
@@ -1,28 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ExamDocument } from 'src/api/exam/db/exam.database.schema';
+import { ExamService } from 'src/api/exam/exam.service';
 import { ProjectsUserService } from 'src/api/projectsUser/projectsUser.service';
-import type { ProjectRank } from './models/home.team.model';
+import { TeamService } from 'src/api/team/team.service';
+import { DateRangeService } from 'src/dateRange/dateRange.service';
+import type {
+  ExamResult,
+  ExamResultDateRanged,
+  ProjectRank,
+} from './models/home.team.model';
 
 @Injectable()
 export class HomeTeamService {
-  constructor(private projectsUserService: ProjectsUserService) {}
-
-  async temp() {
-    return {
-      lastExamResult: {
-        data: [
-          { rank: 2, passCount: 9, totalCount: 20 },
-          { rank: 3, passCount: 3, totalCount: 20 },
-          { rank: 4, passCount: 4, totalCount: 12 },
-          { rank: 5, passCount: 8, totalCount: 18 },
-          { rank: 6, passCount: 1, totalCount: 10 },
-        ],
-        start: new Date(),
-        end: new Date(),
-      },
-    };
-  }
+  constructor(
+    private projectsUserService: ProjectsUserService,
+    private teamService: TeamService,
+    private examService: ExamService,
+    private dateRangeService: DateRangeService,
+  ) {}
 
   async currRegisteredCountRanking(limit: number): Promise<ProjectRank[]> {
     return await this.projectsUserService.currRegisteredCountRanking(limit);
   }
+
+  async recentExamResult(after: number): Promise<ExamResultDateRanged> {
+    const recentExams: Pick<
+      ExamDocument,
+      'beginAt' | 'endAt' | 'location' | 'maxPeople' | 'name' | 'projects'
+    >[] = await this.examService.findAll({
+      select: {
+        beginAt: 1,
+        endAt: 1,
+        location: 1,
+        maxPeople: 1,
+        name: 1,
+        projects: 1,
+      },
+      sort: { beginAt: -1 },
+      skip: Math.max(after - 2, 0),
+      limit: isFirstExam(after) ? 1 : 2,
+    });
+
+    if (!recentExams.length) {
+      throw new NotFoundException();
+    }
+
+    const [firstExam, secondExam] = recentExams;
+
+    const targetBeginAt = secondExam ? secondExam.beginAt : firstExam.beginAt;
+
+    const nextBeginAt = secondExam ? firstExam.beginAt : undefined;
+
+    const targetProjects = secondExam
+      ? secondExam.projects
+      : firstExam.projects;
+
+    const projectIds = targetProjects.map((item) => item.id);
+
+    const resultPerRank = await this.teamService.examResult(
+      targetBeginAt,
+      nextBeginAt,
+      projectIds,
+    );
+
+    const nbrSubscribers = resultPerRank.reduce(
+      (acc, curr) => acc + curr.rate.total,
+      0,
+    );
+
+    const {
+      beginAt,
+      endAt,
+      location,
+      maxPeople = 0,
+      name,
+    } = secondExam ?? firstExam;
+
+    const result: ExamResult = {
+      resultPerRank,
+      nbrSubscribers,
+      beginAt,
+      endAt,
+      location,
+      maxPeople,
+      name,
+    };
+
+    return this.dateRangeService.toDateRanged(result, {
+      start: beginAt,
+      end: endAt,
+    });
+  }
 }
+
+const isFirstExam = (after: number): boolean => after === 1;
