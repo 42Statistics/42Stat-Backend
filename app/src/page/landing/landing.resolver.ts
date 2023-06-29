9b20acbe54853d46f8c114159b4afa8464f5a407
niamu01 <niamu01@naver.com>
2023-06-29 16:25:43 +0900

    feat: :sparkles: HttpException 설정
    - #200

--- app/src/page/landing/landing.resolver.ts
+++ app/src/page/landing/landing.resolver.ts
@@ -1,7 +1,10 @@
+import { UseFilters } from '@nestjs/common';
 import { Query, Resolver } from '@nestjs/graphql';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { LandingService } from './landing.service';
 import { Landing } from './models/landing.model';
 
+@UseFilters(HttpExceptionFilter)
 @Resolver((_of: unknown) => Landing)
 export class LandingResolver {
   constructor(private landingService: LandingService) {}
--- app/src/page/projectInfo/projectInfo.resolver.ts
+++ app/src/page/projectInfo/projectInfo.resolver.ts
@@ -1,7 +1,10 @@
+import { UseFilters } from '@nestjs/common';
 import { Args, Query, Resolver } from '@nestjs/graphql';
+import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { ProjectInfo } from './models/projectInfo.model';
 import { ProjectInfoService } from './projectInfo.service';
 
+@UseFilters(HttpExceptionFilter)
 @Resolver((_of: unknown) => ProjectInfo)
 export class ProjectInfoResolver {
   constructor(private projectInfoService: ProjectInfoService) {}
