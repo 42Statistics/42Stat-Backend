659518779ea8d3950663ee62c53a0b16cd20be8f
niamu01 <niamu01@naver.com>
2023-07-04 16:19:56 +0900

    feat: :sparkles: google연동, 연동해제 추가
    style: :art: 파일명변경 models -> model
    feat: :sparkles: unionType 적용
    feat: :sparkles: 구글 계정이 연동되어있을 시 연동한 시간 & 이메일도 함께 저장
    
    - #210

--- app/src/login/login.resolver.ts
+++ app/src/login/login.resolver.ts
@@ -1,17 +1,30 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { Tokens } from './models/login.models';
-import { AuthService } from './login.service';
-import { GoogleLoginInput } from './login.dto';
+import { GoogleLoginInput } from './dtos/login.dto';
+import { LoginService } from './login.service';
+import { GoogleUser, StatusType, Tokens } from './models/login.model';
 
 @Resolver()
-export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+export class LoginResolver {
+  constructor(private readonly loginService: LoginService) {}
 
-  @Mutation((_returns) => Tokens)
+  @Mutation((_returns) => [StatusType])
   async login(
     @Args('code', { nullable: true }) code?: string,
     @Args('google', { nullable: true }) google?: GoogleLoginInput,
-  ): Promise<Tokens> {
-    return await this.authService.login({ code, google });
+  ): Promise<(typeof StatusType)[]> {
+    return await this.loginService.login({ code, google });
+  }
+
+  @Mutation((_returns) => Boolean)
+  async linkGoogle(
+    @Args('userId') userId: number,
+    @Args('google') google: GoogleLoginInput,
+  ): Promise<boolean> {
+    return await this.loginService.linkGoogle(userId, google);
+  }
+
+  @Mutation((_returns) => Boolean)
+  async unlinkGoogle(@Args('userId') userId: number): Promise<boolean> {
+    return await this.loginService.unlinkGoogle(userId);
   }
 }
