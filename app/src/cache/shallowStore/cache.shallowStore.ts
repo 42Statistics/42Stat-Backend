182fbd90b578520fef72f527a732b23d66b01625
jaham <jpham0005@gmail.com>
2023-07-07 12:54:38 +0900

    feat: :sparkles: ShallowStore 를 이용하는 cache-manager 추가
    기본 In-memory store 는 structured clone 으로 복사 가능한 것들만 저장 가능한데, ObjectId 는 그렇지 않기 때문에 직접 만들었습니다.
    성능상 약간 손해가 생기지만 현재 app 수준에선 괜찮다고 판단했습니다.
    
    - #214

---
+++ app/src/cache/shallowStore/cache.shallowStore.ts
@@ -0,0 +1,50 @@
+import type { CacheStore } from '@nestjs/cache-manager';
+import type { Milliseconds } from 'cache-manager';
+import { LRUCache } from 'lru-cache';
+
+const clone = <T>(value: T): object => {
+  if (typeof value === 'object') {
+    return Object.create(
+      Object.getPrototypeOf(value),
+      Object.getOwnPropertyDescriptors(value),
+    ) as object;
+  }
+
+  return value as object;
+};
+
+export class ShallowStore implements CacheStore {
+  private readonly lruCache;
+  private readonly lruOpts;
+
+  constructor(args?: { max?: number; ttl?: Milliseconds }) {
+    this.lruOpts = {
+      ttlAutopurge: true,
+      max: args?.max ?? 500,
+      ttl: args?.ttl ?? 0,
+    };
+
+    this.lruCache = new LRUCache(this.lruOpts);
+  }
+
+  set = <T>(key: string, value: T, options?: number): Promise<void> => {
+    if ((value ?? null) === null) {
+      throw new Error('wrong value');
+    }
+
+    this.lruCache.set(key, clone(value), {
+      ttl: options ?? this.lruOpts.ttl,
+    });
+
+    return Promise.resolve();
+  };
+
+  get = <T>(key: string): Promise<T | undefined> => {
+    return Promise.resolve(this.lruCache.get(key) as T);
+  };
+
+  del = (key: string): Promise<void> => {
+    this.lruCache.delete(key);
+    return Promise.resolve();
+  };
+}
