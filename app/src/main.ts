2b9a8792848afd8d84744dbeeb56d4d6deb54019
jaham <jpham0005@gmail.com>
2023-07-11 14:54:48 +0900

    feat: :sparkles: enableCors 추가
    기존엔 같은 origin 에서 요청을 보내는 구조였는데, 이것이 달라지면서 추가하게 됐습니다.

--- app/src/main.ts
+++ app/src/main.ts
@@ -6,6 +6,14 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'https://stat.42seoul.kr',
+      'https://dacx6qkh9yupq.cloudfront.net',
+      'https://localhost:8080',
+    ],
+    methods: ['POST'],
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(80);
 }
