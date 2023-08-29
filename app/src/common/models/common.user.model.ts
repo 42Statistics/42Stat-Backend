6a89756bd7e2bf3b500c977bb5760c4960f5850a
jaham <jpham0005@gmail.com>
2023-08-29 15:35:47 +0900

    test: :test_tube: teamService 의 destinyRanking test 추가
    - #311

--- app/src/common/models/common.user.model.ts
+++ app/src/common/models/common.user.model.ts
@@ -1,16 +1,28 @@
 import { Field, Float, ObjectType, PickType } from '@nestjs/graphql';
 import { UserProfile } from 'src/page/personal/general/models/personal.general.userProfile.model';
 import { IndexPaginated } from 'src/pagination/index/models/pagination.index.model';
+import { z } from 'zod';
+
+// todo: userProfile 과 동기화
+export const userPreviewSchema = z.object({
+  id: z.number(),
+  login: z.string(),
+  imgUrl: z.string().nullish(),
+});
+
+export const userRankSchema = z.object({
+  userPreview: userPreviewSchema,
+  value: z.number(),
+  rank: z.number(),
+});
 
 @ObjectType()
-export class UserPreview extends PickType(UserProfile, [
-  'id',
-  'login',
-  'imgUrl',
-]) {}
+export class UserPreview
+  extends PickType(UserProfile, ['id', 'login', 'imgUrl'])
+  implements z.infer<typeof userPreviewSchema> {}
 
 @ObjectType()
-export class UserRank {
+export class UserRank implements z.infer<typeof userRankSchema> {
   @Field((_type) => UserPreview)
   userPreview: UserPreview;
 
