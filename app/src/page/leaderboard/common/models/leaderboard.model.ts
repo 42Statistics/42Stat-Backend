30e98f383ecf1e44eb0e3034173b611d30e759d9
jaham <jpham0005@gmail.com>
2023-09-30 15:16:55 +0900

    refactor: :recycle: leaderboard base 대신 leaderboard metadata 를 사용하도록 변경
    프론트에서 query 하는 방식과 같은 구조로 변경하기 위함입니다.
    
    - close #324

--- app/src/page/leaderboard/common/models/leaderboard.model.ts
+++ app/src/page/leaderboard/common/models/leaderboard.model.ts
@@ -19,11 +19,3 @@ export class LeaderboardElement {
 export class LeaderboardElementDateRanged extends DateRanged(
   LeaderboardElement,
 ) {}
-
-@InterfaceType()
-export abstract class LeaderboardBase {
-  @Field((_type) => [Promo], { description: '기수 정보' })
-  promoList: Promo[];
-
-  // coalition 분류 같은거 넣으면 좋을 듯
-}
--- app/src/page/leaderboard/score/models/leaderboard.score.model.ts
+++ app/src/page/leaderboard/score/models/leaderboard.score.model.ts
@@ -1,10 +1,7 @@
 import { Field, ObjectType } from '@nestjs/graphql';
-import {
-  LeaderboardBase,
-  LeaderboardElementDateRanged,
-} from '../../common/models/leaderboard.model';
+import { LeaderboardElementDateRanged } from '../../common/models/leaderboard.model';
 
-@ObjectType({ implements: () => LeaderboardBase })
+@ObjectType()
 export class LeaderboardScore {
   @Field({
     description: 'TOTAL, CURR_WEEK, CURR_MONTH 만 가능합니다',
