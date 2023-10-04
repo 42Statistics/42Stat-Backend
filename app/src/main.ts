0d6f366e577a8221f9f1b2fbed77051e689306f9
jaham <jpham0005@gmail.com>
2023-10-04 09:03:23 +0900

    build: :bricks: production docker copmose 삭제, port 8080 으로 변경
    기본 80 port 는 system 기본 port 이므로 8080 으로 변경하는게 낫다고 판단했습니다.
    
    - close #343

--- app/src/main.ts
+++ app/src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
     methods: ['POST'],
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(80);
+  await app.listen(8080);
 }
 bootstrap();
