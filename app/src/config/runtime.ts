28951f0985cc3e8a3a003e58564f9b02b766ac40
jaham <jpham0005@gmail.com>
2023-10-07 12:48:01 +0900

    refactor: :recycle: prod, dev, local 을 모두 불러오도록 변경
    - #348

--- app/src/config/runtime.ts
+++ app/src/config/runtime.ts
@@ -4,17 +4,36 @@ import { findEnvByKey } from './util/findEnvByKey';
 type RuntimeConfig = {
   PROD: boolean;
   DEV: boolean;
+  LOCAL: boolean;
   TIMEZONE: string;
   PORT: number;
 };
 
-export const RUNTIME_CONFIG = registerAs('runtime', () => {
+export const RUNTIME_CONFIG_KEY = 'runtime';
+
+export const RUNTIME_CONFIG = registerAs(RUNTIME_CONFIG_KEY, () => {
   let PROD: boolean;
 
   try {
     PROD = findEnvByKey('PROD') === 'true';
   } catch {
     PROD = false;
+  }
+
+  let DEV: boolean;
+
+  try {
+    DEV = findEnvByKey('DEV') === 'true';
+  } catch {
+    DEV = false;
+  }
+
+  let LOCAL: boolean;
+
+  try {
+    LOCAL = findEnvByKey('LOCAL') === 'true';
+  } catch {
+    LOCAL = false;
   }
 
   const PORT = parseInt(findEnvByKey('PORT'));
@@ -24,7 +43,8 @@ export const RUNTIME_CONFIG = registerAs('runtime', () => {
 
   return {
     PROD,
-    DEV: !PROD,
+    DEV,
+    LOCAL,
     TIMEZONE: findEnvByKey('TZ'),
     PORT,
   } satisfies RuntimeConfig;
--- app/src/database/mongoose/database.mongoose.module.ts
+++ app/src/database/mongoose/database.mongoose.module.ts
@@ -1,15 +1,40 @@
 import { Module } from '@nestjs/common';
-import type { ConfigType } from '@nestjs/config';
+import { ConfigService, type ConfigType } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { DATABASE_CONFIG } from 'src/config/database';
+import { type DATABASE_CONFIG, DATABASE_CONFIG_KEY } from 'src/config/database';
+import { type RUNTIME_CONFIG, RUNTIME_CONFIG_KEY } from 'src/config/runtime';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      inject: [DATABASE_CONFIG.KEY],
-      useFactory: (databaseConfig: ConfigType<typeof DATABASE_CONFIG>) => ({
-        uri: databaseConfig.CONNECTION_STRING,
-      }),
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const databaseConfig =
+          configService.getOrThrow<ConfigType<typeof DATABASE_CONFIG>>(
+            DATABASE_CONFIG_KEY,
+          );
+
+        const runtimeConfig =
+          configService.getOrThrow<ConfigType<typeof RUNTIME_CONFIG>>(
+            RUNTIME_CONFIG_KEY,
+          );
+
+        let uri: string;
+
+        if (runtimeConfig.PROD) {
+          uri = databaseConfig.CONNECTION_STRING;
+        } else if (runtimeConfig.DEV) {
+          uri = databaseConfig.DEV_CONNECTION_STRING;
+        } else if (runtimeConfig.LOCAL) {
+          uri = databaseConfig.LOCAL_CONNECTION_STRING;
+        } else {
+          throw Error('Wrong env');
+        }
+
+        return {
+          uri,
+        };
+      },
     }),
   ],
 })
