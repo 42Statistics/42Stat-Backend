5f1b6bd1b5feaafaf16e8c86b940d7347008ac58
jaham <jpham0005@gmail.com>
2023-10-13 07:58:19 +0900

    feat: :sparkles: appspec 설정
    - #356

---
+++ deploy-script/beforeInstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# https://github.com/nvm-sh/nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 18 && nvm use 18 || exit 1
+npm i -g pnpm || exit 1
+mkdir ~/42Stat-Backend
