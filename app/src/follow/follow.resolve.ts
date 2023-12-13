ab0710fb5af3e4971100aaf49e10ac15fd695773
niamu01 <niamu01@naver.com>
2023-12-14 01:50:17 +0900

    feat: :sparkles: followList 타입 생성 및 리스트 함수 추가
    - #405

--- app/src/follow/follow.resolve.ts
+++ app/src/follow/follow.resolve.ts
@@ -3,9 +3,8 @@ import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { MyUserId } from 'src/auth/myContext';
 import { StatAuthGuard } from 'src/auth/statAuthGuard';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
-import { follow } from './db/follow.database.schema';
 import { FollowService } from './follow.service';
-import { FollowSuccess, UserList } from './model/follow.model';
+import { FollowResult, FollowUserList } from './model/follow.model';
 
 @UseFilters(HttpExceptionFilter)
 @UseGuards(StatAuthGuard)
@@ -13,41 +12,47 @@ import { FollowSuccess, UserList } from './model/follow.model';
 export class FollowResolver {
   constructor(private readonly followService: FollowService) {}
 
-  @Mutation((_returns) => FollowSuccess)
+  @Mutation((_returns) => FollowResult)
   async followUser(
     @MyUserId() userId: number,
     @Args('login') login: string,
-  ): Promise<follow> {
+  ): Promise<typeof FollowResult> {
     return await this.followService.followUser(userId, login);
   }
 
-  @Mutation((_returns) => FollowSuccess)
-  async unfollowUser(
+  @Mutation((_returns) => FollowResult)
+  async unFollowUser(
     @MyUserId() userId: number,
     @Args('login') login: string,
-  ): Promise<follow> {
-    return await this.followService.unfollowUser(userId, login);
+  ): Promise<typeof FollowResult> {
+    return await this.followService.unFollowUser(userId, login);
   }
 
-  @Mutation((_returns) => UserList)
-  async getFollowerList(login: string): Promise<UserList> {
-    const user = await this.followService.getFollowerList(login);
+  @Mutation((_returns) => FollowUserList)
+  async getFollowerList(
+    @MyUserId() userId: number,
+    @Args('login') login: string,
+  ): Promise<FollowUserList> {
+    const followUser = await this.followService.getFollowerList(login);
     const count = await this.followService.getFollowerCount(login);
 
     return {
       count,
-      user,
+      followUser,
     };
   }
 
-  @Mutation((_returns) => UserList)
-  async getFollowingList(login: string): Promise<UserList> {
-    const user = await this.followService.getFollowingList(login);
+  @Mutation((_returns) => FollowUserList)
+  async getFollowingList(
+    @MyUserId() userId: number,
+    @Args('login') login: string,
+  ): Promise<FollowUserList> {
+    const followUser = await this.followService.getFollowingList(userId, login);
     const count = await this.followService.getFollowingCount(login);
 
     return {
       count,
-      user,
+      followUser,
     };
   }
 }
