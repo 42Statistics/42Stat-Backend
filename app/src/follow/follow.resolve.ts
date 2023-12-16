58fc13e92a0a381c4d9bce1874b5bfa4eed6a9b3
niamu01 <niamu01@naver.com>
2023-12-16 20:05:35 +0900

    feat: :sparkles: follow/unfollow에 대해 성공했을 시 subscription 추가
    - #405
    
    refactor: :recycle: 프론트 테스트용 임시 함수 리팩토링
    
    - #405
    
    refactor: :recycle: type-only import 추가
    
    - #405
    
    fix: :bug: mutation이 아닌 쿼리에 대해서 mutation -> query 변경
    
    - #405

--- app/src/follow/follow.resolve.ts
+++ app/src/follow/follow.resolve.ts
@@ -1,16 +1,28 @@
 import { UseFilters, UseGuards } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver, Subscription } from '@nestjs/graphql';
+import { PubSub } from 'graphql-subscriptions';
 import { MyUserId } from 'src/auth/myContext';
 import { StatAuthGuard } from 'src/auth/statAuthGuard';
 import { HttpExceptionFilter } from 'src/http-exception.filter';
 import { FollowService } from './follow.service';
 import { FollowListWithCount, FollowResult } from './model/follow.model';
 
+const pubSub = new PubSub();
+
 @UseFilters(HttpExceptionFilter)
-@UseGuards(StatAuthGuard)
 @Resolver()
 export class FollowResolver {
   constructor(private readonly followService: FollowService) {}
+
+  @Subscription((_returns) => FollowResult, {
+    name: 'followUpdated',
+    filter: (payload, _variables) => {
+      return payload.followUpdated.message === 'OK';
+    },
+  })
+  followUpdated() {
+    return pubSub.asyncIterator('followUpdated');
+  }
 
   @Mutation((_returns) => FollowResult, {
     description: '프론트 테스트용 임시 함수',
@@ -23,23 +35,38 @@ export class FollowResolver {
     return await this.followService.MakeFollowUser(to, from, type);
   }
 
+  @UseGuards(StatAuthGuard)
   @Mutation((_returns) => FollowResult)
   async followUser(
     @MyUserId() userId: number,
     @Args('target') target: string,
   ): Promise<typeof FollowResult> {
-    return await this.followService.followUser(userId, target);
+    const followResult = await this.followService.followUser(userId, target);
+
+    if (followResult.message === 'OK') {
+      pubSub.publish('followUpdated', { followUpdated: followResult });
+    }
+
+    return followResult;
   }
 
+  @UseGuards(StatAuthGuard)
   @Mutation((_returns) => FollowResult)
   async unfollowUser(
     @MyUserId() userId: number,
     @Args('target') target: string,
   ): Promise<typeof FollowResult> {
-    return await this.followService.unfollowUser(userId, target);
+    const followResult = await this.followService.unfollowUser(userId, target);
+
+    if (followResult.message === 'OK') {
+      pubSub.publish('followUpdated', { followUpdated: followResult });
+    }
+
+    return followResult;
   }
 
-  @Mutation((_returns) => FollowListWithCount)
+  @UseGuards(StatAuthGuard)
+  @Query((_returns) => FollowListWithCount)
   async getFollowerList(
     @MyUserId() userId: number,
     @Args('target') target: string,
@@ -56,7 +83,8 @@ export class FollowResolver {
     };
   }
 
-  @Mutation((_returns) => FollowListWithCount)
+  @UseGuards(StatAuthGuard)
+  @Query((_returns) => FollowListWithCount)
   async getFollowingList(
     @MyUserId() userId: number,
     @Args('target') target: string,
