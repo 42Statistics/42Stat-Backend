d6684078d24eb1d2625cbc32c137917b65cc9dd0
niamu01 <niamu01@naver.com>
2024-01-20 19:10:47 +0900

    feat: :sparkles: follower, following 구분해 캐시 형태 결정
    - #413
    
    fix: :bug: follow create 안하던 부분 수정

--- app/src/follow/follow.module.ts
+++ app/src/follow/follow.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CursusUserModule } from 'src/api/cursusUser/cursusUser.module';
+import { CacheUtilModule } from 'src/cache/cache.util.module';
 import { PaginationIndexModule } from 'src/pagination/index/pagination.index.module';
 import { FollowSchema, follow } from './db/follow.database.schema';
+import { FollowCacheService } from './follow.cache.service';
 import { FollowResolver } from './follow.resolver';
 import { FollowService } from './follow.service';
 
@@ -11,8 +13,10 @@ import { FollowService } from './follow.service';
     MongooseModule.forFeature([{ name: follow.name, schema: FollowSchema }]),
     CursusUserModule,
     PaginationIndexModule,
+    CacheUtilModule,
   ],
-  providers: [FollowResolver, FollowService],
+  providers: [FollowResolver, FollowService, FollowCacheService],
+  exports: [FollowCacheService],
 })
 // eslint-disable-next-line
 export class FollowModule {}
