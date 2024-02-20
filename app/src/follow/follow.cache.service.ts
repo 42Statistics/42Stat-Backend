63177599a144938daf49a0b317f81a83f7ea02dc
niamu01 <niamu01@naver.com>
2024-02-20 22:05:09 +0900

    refactor: :recycle: 타입, 변수명 변경
    FollowList[] -> Follow[], cache되던 형태의 FollowListCacheType를 Follow로 변경하고 원래 Follow였던 최종 반환 형태를 MyFollow로 변경, 이에 따른 dto파일명이나 pagination 타입도 함께 맞추어 변경
    
    - #413

--- app/src/follow/follow.cache.service.ts
+++ app/src/follow/follow.cache.service.ts
@@ -1,7 +1,7 @@
 import { CACHE_MANAGER } from '@nestjs/cache-manager';
 import { Inject, Injectable } from '@nestjs/common';
 import { CacheUtilService } from 'src/cache/cache.util.service';
-import { FollowListCacheType } from './model/follow.model';
+import { Follow } from './model/follow.model';
 
 export const FOLLOW_LISTS = 'followLists';
 
@@ -19,7 +19,7 @@ export class FollowCacheService {
   }: {
     id: number;
     type: 'follower' | 'following';
-    list: FollowListCacheType[];
+    list: Follow[];
   }): Promise<void> {
     const key = `${id}:${type}:${FOLLOW_LISTS}`;
 
@@ -28,15 +28,10 @@ export class FollowCacheService {
     await this.cacheUtilService.set(key, list, 0);
   }
 
-  async get(
-    userId: number,
-    type: 'follower' | 'following',
-  ): Promise<FollowListCacheType[]> {
+  async get(userId: number, type: 'follower' | 'following'): Promise<Follow[]> {
     const key = `${userId}:${type}:${FOLLOW_LISTS}`;
 
-    const cachedData = await this.cacheUtilService.get<FollowListCacheType[]>(
-      key,
-    );
+    const cachedData = await this.cacheUtilService.get<Follow[]>(key);
 
     if (!cachedData) {
       return []; //todo: 흠,,,
