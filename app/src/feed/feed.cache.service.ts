d1403e8e34fb3afd19a8dd8f448f01807a407f31
niamu01 <niamu01@naver.com>
2024-03-12 23:03:08 +0900

    refactor: :bug: 값이 0일때를 고려해 null처리 변경 || -> ??, push에서 스프레드연산자로 변경
    - #416

--- app/src/feed/feed.cache.service.ts
+++ app/src/feed/feed.cache.service.ts
@@ -8,7 +8,7 @@ export class FeedCacheService {
   private feedCache = new Map<number, (typeof FeedUnion)[]>();
 
   async get(userId: number): Promise<(typeof FeedUnion)[]> {
-    return this.feedCache.get(userId) || [];
+    return this.feedCache.get(userId) ?? [];
   }
 
   async set(userId: number, feed: (typeof FeedUnion)[]): Promise<void> {
@@ -16,8 +16,8 @@ export class FeedCacheService {
   }
 
   async writeFeed(userId: number, feed: typeof FeedUnion): Promise<void> {
-    const cachedFeedList = await this.get(userId);
-    cachedFeedList.push(feed);
-    await this.set(userId, cachedFeedList);
+    const prev = await this.get(userId);
+
+    await this.set(userId, [...prev, feed]);
   }
 }
